2d27bfac822a49d213b4ae17afafa5ce3013222b
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-02-26 16:15:32 +0545

    feat: Created utility functions for authentications

---
+++ src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { API_BASE_URL } from './constants';
+
+
+const apiInstance = axios.create({
+    baseURL: API_BASE_URL,
+    
+    timeout: 100000,
+
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+});
+
+export default apiInstance;
