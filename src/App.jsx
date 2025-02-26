62dea99edab8a9780d2f1e96c5ee221a33d4382b
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-02-26 19:57:55 +0545

    feat: Added authentication components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
+import MainWrapper from './layouts/MainWrapper'
 import Login from './views/auth/Login'
+import Logout from './views/auth/Logout'
+import Register from './views/auth/Register'
+import StoreHeader from './views/base/StoreHeader'
+import StoreFooter from './views/base/StoreFooter'
 
 function App() {
   return (
     <BrowserRouter>
+      <StoreHeader />
       <Routes>
+        <Route path="/" element={<h1>Homepage</h1>} />
         <Route path='/login' element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path='/logout' element={<Logout />} />
       </Routes>
+      <StoreFooter />
     </BrowserRouter>
   )
 }
