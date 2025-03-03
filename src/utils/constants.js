8b404bf68167a84be40cefe4951fe3bfc9c187d7
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-03-03 12:24:31 +0545

    fix: Fixed constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+export const API_BASE_URL = 'https://ecommerce-backend-api.up.railway.app/api/v1/';
 export const PAYPAL_CLIENT_ID = 'AXdjgUoeUoZoH8tEUvDJ_5O-tvcoiPO5eSe_3H5asLnYhVKDsjnd5PRieiVuJ37296dr10RcamapSxx5'
-export const SERVER_URL = 'http://127.0.0.1:8000/'
+export const SERVER_URL = 'https://ecommerce-backend-api.up.railway.app/'
+
+// export const SERVER_URL = 'http://127.0.0.1:8000/'
+// export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
